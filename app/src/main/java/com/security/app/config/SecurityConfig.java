3fb0cf92aff94d808700ed25545460b76abe10c3
Chilaqui <comhector_@hotmail.com>
2025-06-30 12:37:16 -0600

    Confiurando el securityFilterChain

--- app/src/main/java/com/security/app/config/SecurityConfig.java
+++ app/src/main/java/com/security/app/config/SecurityConfig.java
@@ -1,5 +1,34 @@
-package main.java.com.security.app.config;
+package com.security.app.config;
 
+import java.beans.Customizer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .authorizeHttpRequests( auth -> auth
+                .requestMatchers("/auth/admin").hasRole("ROLE_ADMIN")
+                .requestMatchers("/auth/user").hasRole("ROLE_USER")
+                .anyRequest().authenticated()
+            )
+            .formLogin(form -> form.disable());
+        
+        return http.build();
+
+    }
+
+
+
+    
 }
