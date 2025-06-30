0649eb308643376ffc26010f50c32377963e1443
Chilaqui <comhector_@hotmail.com>
2025-06-30 13:07:36 -0600

    Confgurando el user details service con jdbc para que se conecte conla base de datos

--- app/src/main/java/com/security/app/config/SecurityConfig.java
+++ app/src/main/java/com/security/app/config/SecurityConfig.java
@@ -2,10 +2,14 @@ package com.security.app.config;
 
 import java.beans.Customizer;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,6 +30,11 @@ public class SecurityConfig {
         
         return http.build();
 
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        return new  JdbcUserDetailsManager(dataSource);
     }
 
 
