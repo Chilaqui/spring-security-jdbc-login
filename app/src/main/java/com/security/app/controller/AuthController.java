ac9a3bd9f23ede350a32213368b8670cd94111a4
Chilaqui <comhector_@hotmail.com>
2025-06-30 12:36:58 -0600

    Confogurando el controller para los end points que podran entrar

--- app/src/main/java/com/security/app/controller/AuthController.java
+++ app/src/main/java/com/security/app/controller/AuthController.java
@@ -1,5 +1,23 @@
-package main.java.com.security.app.controller;
+package com.security.app.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    @GetMapping("admin")
+    public ResponseEntity<?> adminEntity(){
+        return ResponseEntity.ok("Bien logiado aministrador");
+    }
+
+    @GetMapping("user")
+    public ResponseEntity<?> userEntity(){
+        return ResponseEntity.ok("Bien logiado usuario");
+    }
+
 
 }
