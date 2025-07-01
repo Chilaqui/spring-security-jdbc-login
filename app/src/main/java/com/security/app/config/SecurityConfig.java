f23c88f54161988c518dff933c4de1579637b46a
Chilaqui <comhector_@hotmail.com>
2025-07-01 10:28:32 -0600

    Comentando un EnableMethodSecury  por que no me deja porbar varios endpoit

--- app/src/main/java/com/security/app/config/SecurityConfig.java
+++ app/src/main/java/com/security/app/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+/* @EnableMethodSecurity(prePostEnabled = true) */
 public class SecurityConfig {
 
 
